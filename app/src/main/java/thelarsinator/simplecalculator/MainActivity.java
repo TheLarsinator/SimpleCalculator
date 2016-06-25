e521eb3cc01c63c9cab766b928d061b99fc37131
TheLarsinator <TheLarsinatormodding@hotmail.com>
2016-06-25 21:50:41 +0200

    Comments in main file, and some vcs stuff

--- app/src/main/java/thelarsinator/simplecalculator/MainActivity.java
+++ app/src/main/java/thelarsinator/simplecalculator/MainActivity.java
@@ -19,30 +19,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //Function to start the simple math screen
     public void goToSimpleMath(View view)
     {
         Intent intent = new Intent(this, SimpleMath.class);
         startActivity(intent);
     }
 
+    //Function to start the screen to solve polynomials
     public void goToPolynomials(View view)
     {
         Intent intent = new Intent(this, SolvePolynomials.class);
         startActivity(intent);
     }
 
+    //Function to start trig screen
     public void goToAngle(View view)
     {
         Intent intent = new Intent(this, AngleMath.class);
         startActivity(intent);
     }
 
+    //Function to start complex numbers screen
     public void goToComplex(View view)
     {
         Intent intent = new Intent(this, ComplexMath.class);
         startActivity(intent);
     }
 
+    //Function to start time screen
     public void goToTime(View view)
     {
         Intent intent = new Intent(this, TimeMath.class);
